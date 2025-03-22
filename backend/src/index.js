9d4ad63539ded94835b8242b745483307e3de593
Gabriel Augusto Ribeiro da Silva <gabrielsilva.webdeveloper@gmail.com>
2025-03-22 17:20:54 -0300

    installing cors so that the frontend has permission to access the API backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { connectDB } = require('../db/config');
 const taskRoutes = require('../routes/taskRoute');
 
@@ -7,6 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Configuração do CORS
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    methods: 'GET,POST,PUT,DELETE', 
+    allowedHeaders: 'Content-Type,Authorization' 
+}));
 
 // Routes
 app.use('/api/v1/tasks', taskRoutes);
